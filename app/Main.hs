8125db96ddb46fd2a1c10aee8ff1f2f19ce22222
Charles HD <charleshdespointes@gmail.com>
2016-05-13 19:10:11 +0200

    add context behavior with only one argument

--- app/Main.hs
+++ app/Main.hs
@@ -10,4 +10,7 @@ main = do args <- getArgs
                       input = args !! 1
                       output = args !! 2
                   in extractPalette n input output
-             else putStrLn "Must supply 3 args : size of palette, input file, output file"
+             else if length args == 1 
+	             then let f = head args 
+		          in extractPalette 16 f "palette.bmp"
+		     else putStrLn "Must supply 3 args : size of palette, input file, output file"
